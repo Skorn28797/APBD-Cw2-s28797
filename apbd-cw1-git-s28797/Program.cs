546c663c222ec4038dff5cdd499f48eb15c838ce
Stanisław Pawłowski <s28797@pjwstk.edu.pl>
2026-03-19 23:31:32 +0100

    Add CalculateAverage method

--- apbd-cw1-git-s28797/Program.cs
+++ apbd-cw1-git-s28797/Program.cs
@@ -3,3 +3,10 @@
 int[] numbers = { 1, 2, 3, 4, 10 };
 
 foreach (int n in numbers) {Console.WriteLine("Number: " + n);}
+
+static double CalculateAvarage(int[] numbers)
+{
+    double sum = 0;
+    foreach (var n in numbers) sum += n;
+    return sum/numbers.Length;
+}
